2822eb4ec294ffc27872b694afad00dbb5cb329c
Lost-in-Noise <nancykikvadze@gmail.com>
2023-09-06 12:49:16 +0400

    button delete added from font awesome

--- script.js
+++ script.js
@@ -12,18 +12,25 @@ let btnAdd = document.querySelector(".btn");
 let inputElementToDo = document.querySelector(".input-form");
 let ulElementToDo = document.querySelector(".ul-list");
 let clearBtn = document.querySelector(".clearList");
+let form = document.querySelector('.form-wraper');
 
-
-btnAdd.addEventListener('click',function() {
+form.addEventListener('submit',function(event) {
+    event.preventDefault();
     // 1. get input value
 let valueInput = inputElementToDo.value;
+
+if (valueInput == " ") {
+    return;
+    }
 
     // 2.input value in li - ul
 let li = document.createElement("li");
 
     //წაშლის ღილაკი
-let btnDelete = document.createElement('button');
-btnDelete.textContent = "Delete this Item";
+let btnDelete = document.createElement('i');
+
+btnDelete.classList.add('fa-solid', 'fa-delete-left');
+
 btnDelete.addEventListener("click", function () {
     li.remove();
 });
