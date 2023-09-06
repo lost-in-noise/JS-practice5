7bee31e9b0c6b03a3b0e8c386a2f75ad86808c9e
Lost-in-Noise <nancykikvadze@gmail.com>
2023-09-06 10:21:46 +0400

    added ToDoList in JS

--- script.js
+++ script.js
@@ -7,5 +7,39 @@ document.querySelector('.burger-menu').addEventListener('click', function() {
 
 
 
+//to do list
+let btnAdd = document.querySelector(".btn");
+let inputElementToDo = document.querySelector(".input-form");
+let ulElementToDo = document.querySelector(".ul-list");
+let clearBtn = document.querySelector(".clearList");
+
+
+btnAdd.addEventListener('click',function() {
+    // 1. get input value
+let valueInput = inputElementToDo.value;
+
+    // 2.input value in li - ul
+let li = document.createElement("li");
+
+    //წაშლის ღილაკი
+let btnDelete = document.createElement('button');
+btnDelete.textContent = "Delete this Item";
+btnDelete.addEventListener("click", function () {
+    li.remove();
+});
+
+li.textContent = valueInput;
+li.appendChild(btnDelete);
+
+ulElementToDo.appendChild(li);
+
+    // 3. clear input
+inputElementToDo.value = ' ';
+
+})
+
+clearBtn.addEventListener("click", function () {
+ulElementToDo.innerHTML = " ";
+});
 
 
