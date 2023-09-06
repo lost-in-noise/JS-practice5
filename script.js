a49ac0b793b4ed2993881ef306761dd4374b93ef
Lost-in-Noise <nancykikvadze@gmail.com>
2023-09-07 00:28:29 +0400

    oh mein Got - added burger menu

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 'use strict';
 
-document.querySelector('.burger-menu').addEventListener('click', function() {
-    this.classList.toggle('active');
-    
-});
+//burger bar
+let navBar = document.getElementById('navBar');
+let burgerBar = document.getElementById('burgerBar');
+let navDiv = document.getElementById('navDiv');
+
+burgerBar.addEventListener('click', function() {
+    navBar.classList.toggle('activeNav');
+    burgerBar.classList.toggle('active');
+    navDiv.classList.toggle('mobActive');
+})
 
 
 
